7dbf68c94d054bf2761cae2cb5fcb66cb5442b2f
Pritam Dhara <pritam1813@gmail.com>
2023-08-26 16:50:41 +0530

    Added Profile Page UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,5 +1,69 @@
-import React from "react";
+import { useSelector } from "react-redux";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function Profile() {
-  return <div>Profile</div>;
+  const { currentUser } = useSelector((state) => state.user);
+  const [formData, setFormData] = useState({});
+  const { loading, error } = useSelector((state) => state.user);
+
+  const handleInputChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+    } catch (error) {}
+  };
+
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Profile</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <img
+          src={currentUser.avatar}
+          alt="Profile Avatar"
+          className="h-24 w-24 self-center cursor-pointer rounded-full object-cover "
+        />
+        <input
+          defaultValue={currentUser.username}
+          type="text"
+          placeholder="username"
+          id="username"
+          className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleInputChange}
+        />
+        <input
+          defaultValue={currentUser.email}
+          type="email"
+          placeholder="email"
+          id="email"
+          className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleInputChange}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          id="password"
+          className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleInputChange}
+        />
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg
+    uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "Loading..." : "Update"}
+        </button>
+      </form>
+      <div className="flex justify-between mt-5">
+        <span className="text-red-700 cursor-pointer">Delete Account</span>
+        <Link to="/signout">
+          <span className="text-blue-500">Sign Out</span>
+        </Link>
+      </div>
+      <p className="text-red-700 mt-5">{error && "Something Went Wrong"}</p>
+    </div>
+  );
 }
