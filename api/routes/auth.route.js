19659752a9599f4d084f3d3a1042d655016d2d51
Pritam Dhara <pritam1813@gmail.com>
2023-08-26 10:08:06 +0530

    created login api route

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { auth, signup } from "../controllers/auth.controller.js";
+import { auth, signin, signup } from "../controllers/auth.controller.js";
 const router = express.Router();
 
 router.get("/", auth);
 router.post("/signup", signup);
+router.post("/login", signin);
 
 export default router;
